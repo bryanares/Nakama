22f8efbbe4d81f5b5d439d7a6fa3a4d6dcca87e3
bryanares <bryanares12@gmail.com>
2023-07-11 14:41:27 +0300

    implement viewBinding

--- app/src/main/java/com/example/nakama/MainActivity.kt
+++ app/src/main/java/com/example/nakama/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.nakama
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nakama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
     }
 }
