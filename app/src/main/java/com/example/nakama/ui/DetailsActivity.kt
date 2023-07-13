4207dc026ccb1ee73c82eee7608bc003f62f4ce9
bryanares <bryanares12@gmail.com>
2023-07-13 13:45:19 +0300

    set upintent

--- app/src/main/java/com/example/nakama/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/nakama/ui/DetailsActivity.kt
@@ -1,12 +1,27 @@
 package com.example.nakama.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.nakama.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nakama.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //declare screen properties
+        val animeImage = binding.detailImageView
+        val animeTitle = binding.detailTitleTv
+        val animeDescription = binding.detailDescriptionTv
+        val animeRating = binding.detailRatingTv
+
+        //receive the intent
+//        intent.getStringExtra("image")?.let { animeImage.setImageResource(it.toInt()) }
+        animeTitle.text = intent.getStringExtra("title").toString()
+        animeDescription.text = intent.getStringExtra("description").toString()
+        animeRating.text = intent.getStringExtra("rating").toString()
     }
 }
