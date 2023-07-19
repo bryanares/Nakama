84fb53d035cfb44f93aadb237cac0d71dddee6d3
bryanares <bryanares12@gmail.com>
2023-07-19 13:21:30 +0300

    build network layer

--- app/src/main/java/com/example/nakama/data/api/AnimeApi.kt
+++ app/src/main/java/com/example/nakama/data/api/AnimeApi.kt
@@ -1,4 +1,15 @@
 package com.example.nakama.data.api
 
-class AnimeApi {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    //instance of the api class
+    val api: AnimeApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.jikan.moe/v4/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AnimeApi::class.java)
+    }
 }
--- app/src/main/java/com/example/nakama/data/api/ApiService.kt
+++ app/src/main/java/com/example/nakama/data/api/ApiService.kt
@@ -1,2 +1,10 @@
-package com.example.nakama.data.api 
+package com.example.nakama.data.api
 
+import com.example.nakama.data.models.anime
+import retrofit2.http.GET
+
+interface AnimeApi {
+
+    @GET("random/anime")
+    suspend fun getAnime(): anime
+}
