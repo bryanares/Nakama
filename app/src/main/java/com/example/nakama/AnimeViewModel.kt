76bff07237db9e68979218ce929c13eef643770e
bryanares <bryanares12@gmail.com>
2023-07-19 13:23:31 +0300

    launch coroutine to fetch data using api service

--- app/src/main/java/com/example/nakama/AnimeViewModel.kt
+++ app/src/main/java/com/example/nakama/AnimeViewModel.kt
@@ -1,4 +1,37 @@
-package com.example.nakama.data
+package com.example.nakama
 
-class AnimeViewModel {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nakama.data.api.RetrofitInstance
+import com.example.nakama.data.models.anime
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class AnimeViewModel : ViewModel() {
+
+    private var _anime = MutableLiveData<anime>()
+    val anime :LiveData<anime> get() = _anime
+
+//    val animeItem = anime.value?.data
+    init {
+        getAnime()
+    }
+
+    fun getAnime() : MutableLiveData<anime>{
+        viewModelScope.launch {
+            try {
+                _anime.value = RetrofitInstance.api.getAnime()
+                Log.d("viewModel", anime.value?.data.toString())
+            }catch (e: Exception){
+//                Log.e("ViewModel", e.toString())
+
+            }
+        }
+        return _anime
+    }
+
+
 }
